f543ae1f8585b494df4b962434d89d5a7b96e8e9
ocnyang <ocnyang@gmail.com>
2024-03-08 15:25:01 +0800

    ---;

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 222.0,
-          alignment: Alignment.center,
-          child: Card.filled(
-            color: Colors.white,
-            elevation: 0.315,
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: Card.filled(
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(9.0))),
+          color: Colors.black,
+          elevation: 3,
+          child: IntrinsicWidth(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
@@ -50,15 +51,54 @@ class _MyHomePageState extends State<MyHomePage> {
                     data: ThemeData(fontFamily: 'Chappa'),
                     child: const Text(
                       'deeppoor',
-                      style: TextStyle(color: Colors.black, fontFamily: 'Chappa', fontSize: 24.0),
+                      style: TextStyle(color: Colors.white, fontFamily: 'Chappa', fontSize: 24.0),
                     ),
                   ),
                 ),
-                const Divider(height: 15.0, color: Colors.black12),
-                Container(
-                  padding: const EdgeInsets.only(bottom: 10.0, left: 18.0, right: 18.0),
-                  alignment: Alignment.centerRight,
-                  child: Text("穷奇", style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Colors.black)),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Divider(height: .0, color: Colors.white),
+                IntrinsicHeight(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                          alignment: Alignment.center,
+                          child: const Icon(
+                            Icons.emoji_transportation,
+                            color: Colors.white,
+                            size: 18.0,
+                          ),
+                        ),
+                      ),
+                      const VerticalDivider(color: Colors.white, width: 0.0),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                          alignment: Alignment.center,
+                          child: const Icon(
+                            Icons.savings,
+                            color: Colors.white,
+                            size: 18.0,
+                          ),
+                        ),
+                      ),
+                      const VerticalDivider(color: Colors.white, width: 0.0),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                          alignment: Alignment.center,
+                          child: const Icon(
+                            Icons.face_3,
+                            color: Colors.white,
+                            size: 18.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
